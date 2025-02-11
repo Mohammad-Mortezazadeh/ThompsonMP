c113da6f4408628823125fec07e98f9444479a65
Mohammad Mortezazadeh Dorostkar <mohammad.mortezazadeh-dorostkar@ec.gc.ca>
2025-02-11 18:13:30 +0000

    Single precision for all variables

--- ref/src/machine.F90
+++ ref/src/machine.F90
@@ -1,51 +1,21 @@
-      module machine
+module machine
 
-!! \section arg_table_machine
-!! \htmlinclude machine.html
-!!
+  use iso_fortran_env, only: int32, real32  ! Use single precision from ISO_FORTRAN_ENV
 
-      implicit none
+  implicit none
 
-#ifndef SINGLE_PREC
-      integer, parameter :: kind_io4  = 4, kind_io8  = 8 , kind_ior = 8 &
-     &,                     kind_evod = 8, kind_dbl_prec = 8            &
-#ifdef __PGI
-     &,                     kind_qdt_prec = 8                           &
-#else
-     &,                     kind_qdt_prec = 16                          &
-#endif
-     &,                     kind_rad  = 8                               &
-     &,                     kind_phys = 8     ,kind_taum=8              &
-     &,                     kind_grid = 8                               &
-     &,                     kind_REAL = 8                               &! used in cmp_comm
-     &,                     kind_LOGICAL = 4                            &
-     &,                     kind_INTEGER = 4                             ! -,,-
+  integer, parameter :: kind_io4 = int32, kind_io8 = int32, kind_ior = int32
+  integer, parameter :: kind_evod = real32, kind_dbl_prec = real32
+  integer, parameter :: kind_qdt_prec = real32
+  integer, parameter :: kind_rad = real32, kind_phys = real32, kind_taum = real32
+  integer, parameter :: kind_grid = real32
+  integer, parameter :: kind_REAL = real32  ! Used in cmp_comm
+  integer, parameter :: kind_LOGICAL = int32
+  integer, parameter :: kind_INTEGER = int32
 
-#else
-      integer, parameter :: kind_io4  = 4, kind_io8  = 8 , kind_ior = 8 &
-     &,                     kind_evod = 4, kind_dbl_prec = 8            &
-#ifdef __PGI
-     &,                     kind_qdt_prec = 8                           &
-#else
-     &,                     kind_qdt_prec = 16                          &
-#endif
-     &,                     kind_rad  = 4                               &
-     &,                     kind_phys = 4     ,kind_taum=4              &
-     &,                     kind_grid = 4                               &
-     &,                     kind_REAL = 4                               &! used in cmp_comm
-     &,                     kind_LOGICAL = 4                            &
-     &,                     kind_INTEGER = 4                             ! -,,-
+  integer, parameter :: kind_dyn = real32
 
-#endif
+  real(kind=kind_evod), parameter :: mprec = 1.0e-6_real32  ! Machine precision for single precision
+  real(kind=kind_evod), parameter :: grib_undef = 9.99e20_real32  ! GRIB undefined value
 
-#ifdef OVERLOAD_R4
-      integer, parameter :: kind_dyn  = 4 
-#else
-      integer, parameter :: kind_dyn  = 8
-#endif
-
-!
-      real(kind=kind_evod), parameter :: mprec = 1.e-12           ! machine precision to restrict dep
-      real(kind=kind_evod), parameter :: grib_undef = 9.99e20     ! grib undefine value
-!
-      end module machine
+end module machine
